40717010a2de83da3ca47b441d6434c23671ac02
Michal Ciberej <michal.ciberej@gmail.com>
2023-10-28 20:13:30 +0200

    Added types

---
+++ typings.d.ts
@@ -0,0 +1,34 @@
+type Links = {
+  title: string;
+  link: string;
+};
+
+export interface ProjectProps {
+  thumbnail: string;
+  header: string;
+  alt: string;
+  description: string[];
+  techs: string[];
+  left: boolean;
+  links: Links[];
+}
+
+export interface StyledLinkProps {
+  link: string;
+  currentSection?: string;
+  id?: string;
+  icon?: boolean;
+  black?: boolean;
+  border?: boolean;
+  target?: boolean;
+  children: string;
+}
+
+export interface StyledButtonProps {
+  submit?: boolean;
+  black?: boolean;
+  border?: boolean;
+  func?: (e: any) => void;
+  children: string;
+  icon?: boolean;
+}
