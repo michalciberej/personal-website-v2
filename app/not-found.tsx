fc5c017c1ebca635dade4d5ae8f6a5843448101b
Michal Ciberej <michal.ciberej@gmail.com>
2023-10-29 19:39:21 +0200

    Refactored responsive styles

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Metadata } from 'next';
+import Link from 'next/link';
+import { IoIosArrowRoundBack } from 'react-icons/io';
+import Footer from './components/Footer';
 
 export const metadata: Metadata = {
   title: 'Something went wrong | Michal Ciberej',
@@ -8,15 +11,29 @@ export const metadata: Metadata = {
 
 const notFoundPage = () => {
   return (
-    <main>
+    <main className='h-[100dvh] bg-backgroundD px-[4vw] md:px-[10vw] text-textD flex flex-col justify-between'>
+      <header>
+        <div className='pt-6'>
+          <Link
+            href='/'
+            aria-label='back to homepage'
+            className='group hover:text-primary transition-all flex items-center'>
+            <IoIosArrowRoundBack className='text-4xl group-hover:-translate-x-2 md:group-hover:-translate-x-4 transition-transform' />
+            <span className='text-xl md:text-2xl'>Back to homepage</span>
+          </Link>
+        </div>
+      </header>
       <section>
-        <div className='h-[100dvh] bg-backgroundD text-textD flex flex-col justify-center items-center'>
-          <h1 className='text-9xl bg-clip-text bg-gradient-to-tr from-primary to-secondary text-transparent'>
+        <div className='text-textD flex flex-col justify-center items-center'>
+          <h1 className='text-7xl md:text-9xl bg-clip-text bg-gradient-to-tr from-primary to-secondary text-transparent'>
             OH NO
           </h1>
-          <p className='text-2xl'>Something went wrong</p>
+          <p className='text-xl md:text-2xl'>Something went wrong</p>
         </div>
       </section>
+      <footer>
+        <Footer white />
+      </footer>
     </main>
   );
 };
--- app/thankyou/page.tsx
+++ app/thankyou/page.tsx
@@ -12,23 +12,23 @@ export const metadata: Metadata = {
 
 const ThankYouPage = () => {
   return (
-    <main className='h-[100dvh] bg-backgroundD px-[10vw] text-textD flex flex-col justify-between'>
+    <main className='h-[100dvh] bg-backgroundD px-[4vw] md:px-[10vw] text-textD flex flex-col justify-between'>
       <header>
         <div className='pt-6'>
           <Link
             href='/'
             aria-label='back to homepage'
             className='group hover:text-primary transition-all flex items-center'>
-            <IoIosArrowRoundBack className='text-4xl group-hover:-translate-x-4 transition-transform' />
-            <span className='text-2xl'>Back to homepage</span>
+            <IoIosArrowRoundBack className='text-4xl group-hover:-translate-x-2 md:group-hover:-translate-x-4 transition-transform' />
+            <span className='text-xl md:text-2xl'>Back to homepage</span>
           </Link>
         </div>
       </header>
       <section>
-        <div className='flex flex-col items-center justify-center text-3xl -mt-10 space-y-4'>
-          <LuHeartHandshake className='text-6xl text-primary' />
-          <h1>Thank you for reaching out.</h1>
-          <p className='text-xl'>Will get to you as soon as possible.</p>
+        <div className='flex flex-col items-center justify-center -mt-10 space-y-4'>
+          <LuHeartHandshake className='text-5xl md:text-6xl text-primary' />
+          <h1 className='text-2xl md:text-3xl'>Thank you for reaching out.</h1>
+          <p className='md:text-xl'>Will get to you as soon as possible.</p>
         </div>
       </section>
       <footer>
